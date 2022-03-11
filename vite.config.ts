a7fc67629313f944672fad92271e7faee037a2a4
BackRunner <dev@backrunner.top>
2022-03-11 13:13:42 +0800

    feat: add demo build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,39 @@ import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), dts()],
-  build: {
-    sourcemap: true,
-    minify: 'terser',
-    outDir: 'lib',
-    lib: {
-      entry: path.resolve(__dirname, './src/index.ts'),
-      name: 'markvue',
-      formats: ['es', 'umd'],
-    },
-    rollupOptions: {
-      external: ['vue', 'marked', '@vue/compiler-sfc'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          marked: 'Marked',
-          '@vue/compiler-sfc': 'compilerSfc',
+const BuildConfig = {
+  lib: {
+    plugins: [vue(), dts()],
+    build: {
+      sourcemap: true,
+      minify: 'terser' as 'terser',
+      outDir: 'lib',
+      lib: {
+        entry: path.resolve(__dirname, './src/index.ts'),
+        name: 'markvue',
+        formats: ['es', 'umd'],
+      },
+      rollupOptions: {
+        external: ['vue', 'marked', '@vue/compiler-sfc'],
+        output: {
+          globals: {
+            vue: 'Vue',
+            marked: 'Marked',
+            '@vue/compiler-sfc': 'compilerSfc',
+          },
         },
       },
     },
   },
-});
+  demo: {
+    plugins: [vue()],
+    build: {
+      sourcemap: true,
+      minify: 'terser' as 'terser',
+      outDir: 'dist',
+    },
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(process.env.BUILD_TARGET === 'demo' ? BuildConfig.demo : BuildConfig.lib);
